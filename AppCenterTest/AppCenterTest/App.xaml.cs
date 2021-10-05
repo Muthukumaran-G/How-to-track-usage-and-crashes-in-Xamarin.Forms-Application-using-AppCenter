16d612c366123141e326b230902f00c88267725c
Muthukumaran-G <muthukumaran.gnanavinayagam@syncfusion.com>
2021-10-05 21:18:33 +0530

    Included demo.

---
+++ AppCenterTest/AppCenterTest/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCenterTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            AppCenter.Start("android=389520bb-8081-4d7a-9794-8e5f0ca3f2a8;" +
+                  "uwp=9f362fec-af4c-4391-a007-75ee5e2076c7;" +
+                  "ios=e0cf9666-af05-46d5-806a-92cfb277e89e",
+                  typeof(Analytics), typeof(Crashes));
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppCenterTest/AppCenterTest/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AppCenter.Crashes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCenterTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Crashes.GenerateTestCrash();
+            }
+            catch (Exception exception)
+            {
+                Crashes.TrackError(exception);
+            }
+            
+        }
+    }
+}
